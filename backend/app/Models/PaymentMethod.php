e4e72f6bd857a08166a4f53deb4a524f0567b22a
Zolaet <zolacrypt@gmail.com>
2026-09-04 20:24:50 +0300

    payment_method model

--- backend/app/Models/PaymentMethod.php
+++ backend/app/Models/PaymentMethod.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PaymentMethod extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'type',
+        'account_name',
+        'account_number',
+        'bank_or_provider_name',
+        'instructions',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
